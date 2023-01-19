d3ab33ce907235e4a9f055a344ad7205f0a5f8ac
Ekaterina <missathlon08@gmail.com>
2023-01-19 23:09:51 +0300

    Lesson 5

--- Lesson_4/HW/Task_4_4.py
+++ Lesson_4/HW/Task_4_4.py
@@ -44,3 +44,26 @@ k = int(input("Введите натуральную степень k = "))
 list_0 = list(range(0, 10))
 
 create_str(k, list_0)
+
+# from random import choice, sample
+
+
+# def polynomial(num: int):
+#     if num < 1:
+#         return 0
+
+#     poly = ""
+#     num_list = range(0, 11)
+
+#     with open("poly_2.txt", "a", encoding="utf-8") as my_f:
+#         for i in range(num, 1, -1):
+#             value = choice(num_list)
+#             if value:
+#                 poly += f"{value}*x^{i} {choice('+-')} "
+
+#         numbers = sample(range(1, 11), k=2)
+#         my_f.write(f"{poly}{choice(numbers)}*x {choice('+-')} {choice(numbers)} = 0\n")
+
+
+# for _ in range(3):
+#     polynomial(int(input()))
--- Lesson_4/HW/Task_4_5.py
+++ Lesson_4/HW/Task_4_5.py
@@ -28,3 +28,24 @@ with open("str_res.txt", "w", encoding="utf-8") as file:
         st2 = open('str_1.txt').read().split('\n')[i]
         str2 = str(st2)
         file.write(f"{str1[0:st_1_len-4]} + {str2}\n")
+
+
+# from random import choice
+
+
+# def poly_sum(name_1: str, name_2: str):
+#     with open(name_1, "r", encoding="utf-8") as my_f_1, \
+#             open(name_2, "r", encoding="utf-8") as my_f_2:
+#         first = my_f_1.readlines()
+#         second = my_f_2.readlines()
+
+#         if len(first) == len(second):
+#             with open("sum_poly.txt", "a", encoding="utf-8") as my_f_3:
+#                 for i, v in enumerate(first):
+#                     my_f_3.write(f"{v[:-5]} + {second[i]}")
+#         else:
+#             print("The contents of the files do not match!")
+
+
+# # poly_sum(input("Enter the file name 'text_1.txt': "), input("Enter the file name 'text_2.txt': "))
+# poly_sum("poly.txt", "poly_2.txt")
